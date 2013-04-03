cca75851c3b81a0744ed82f36bbeb5d65ecb9d74
Philihp Busby <philihp@gmail.com>
2013-04-03 02:04:40 -0400

    Revert "can't get dust to render"
    This reverts commit 882729e2c266f6e8b7879fe47441b0a245041c69.

--- app/assets/javascripts/labora.coffee
+++ app/assets/javascripts/labora.coffee
@@ -2,5 +2,4 @@ $ ->
   jsRoutes.controllers.LaboraGame.index().ajax success: (games) ->
     $.each games, (index, game) ->
       console.log('data = ' + JSON.stringify(game))
-      dust.render('game', game)
-#      $('#games').append $('<li>').text game.id
+      $('#games').append $('<li>').text game.id
--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,6 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "be.objectify"  %%  "deadbolt-java"     % "2.1-SNAPSHOT",
-      // Comment this for local development of the Play Authentication core
       "com.feth"      %%  "play-authenticate" % "0.2.5-SNAPSHOT",
       "mysql" % "mysql-connector-java" % "5.1.18",
       javaCore,
@@ -18,13 +17,14 @@ object ApplicationBuild extends Build {
     )
     
     val main = play.Project(appName, appVersion, appDependencies).settings(
+      
       resolvers += Resolver.url("Objectify Play Repository (release)", url("http://schaloner.github.com/releases/"))(Resolver.ivyStylePatterns),
       resolvers += Resolver.url("Objectify Play Repository (snapshot)", url("http://schaloner.github.com/snapshots/"))(Resolver.ivyStylePatterns),
       resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns),
       resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns),
       resolvers += Resolver.url("play-authenticate (release)", url("http://joscha.github.com/play-authenticate/repo/releases/"))(Resolver.ivyStylePatterns),
       resolvers += Resolver.url("play-authenticate (snapshot)", url("http://joscha.github.com/play-authenticate/repo/snapshots/"))(Resolver.ivyStylePatterns),
-	  resolvers += Resolver.url("Typesafe Ivy Snapshots Repository", url("http://repo.typesafe.com/typesafe/ivy-snapshots/"))(Resolver.ivyStylePatterns)
+	  resolvers += Resolver.url("Typesafe Ivy Snapshots Repository", url("http://repo.typesafe.com/typesafe/ivy-snapshots/"))(Resolver.ivyStylePatterns),
       templatesImport += "be.objectify.deadbolt.java.views.html._",
       templatesImport += "be.objectify.deadbolt.core.utils.TemplateUtils._"
     )
